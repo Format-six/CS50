2817d60a4d79aee926c424a83622f878c4f47976
format-six <format.six.for@gmail.com>
2026-02-28 19:02:50 +0800

    Week2 Scrabble change

--- Week2/Scrabble/Scrabble.c
+++ Week2/Scrabble/Scrabble.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 
-int Score(char *Word);
+int Score(string Word);
 
 int main()
 {
@@ -24,7 +24,7 @@ int main()
     return 0;
 }
 
-int Score(char *Word)
+int Score(string Word)
 {
     int score = 0,table[26] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
     for(int a=0,len = strlen(Word);a < len;a++)
